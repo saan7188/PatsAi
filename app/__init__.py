c3ea443d3c843adeb6ca42a8e558273d26a7b318
saan7188 <ssksanjay66@gmail.com>
2025-08-05 09:04:07 +0530

    Update and rename app.py to app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'  # Replace or set in .env
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/patai.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    from app.models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    from app.routes import main_bp
+    from app.auth import auth_bp
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    return app
